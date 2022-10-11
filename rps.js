3787e25a4dcafe6b0389381ec41ca28408cb6375
Peter Katz <peter.nichols.katz@gmail.com>
2022-10-11 14:43:12 -0400

    update playRound function

--- rps.js
+++ rps.js
@@ -1,5 +1,8 @@
 const myArray = ["Rock","Paper","Scissors"];
 
+const playerSelection  = prompt("Rock, Paper, or Scissors?", " ");
+console.log(playerSelection);
+
 function getComputerChoice () {
     return myArray[Math.floor(Math.random() * myArray.length)];
 }
@@ -8,5 +11,11 @@ console.log(getComputerChoice());
 function playRound(playerSelection, computerSelection){
     computerSelection = getComputerChoice().toLowerCase();
     playerSelection = playerSelection.toLowerCase();
-    
+
+    if(playerSelection == computerSelection){
+        alert(
+            `It's a tie!`
+        );
+    }
+
 }
