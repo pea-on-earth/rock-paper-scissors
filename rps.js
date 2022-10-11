c01d9ef0c2fb533eefe95dec04668dd5e85011e3
Peter Katz <peter.nichols.katz@gmail.com>
2022-10-11 14:27:58 -0400

    add initial functions

--- rps.js
+++ rps.js
@@ -4,3 +4,9 @@ function getComputerChoice () {
     return myArray[Math.floor(Math.random() * myArray.length)];
 }
 console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection){
+    computerSelection = getComputerChoice().toLowerCase();
+    playerSelection = playerSelection.toLowerCase();
+    
+}
