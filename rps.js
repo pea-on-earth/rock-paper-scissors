5e1ad6323a2a331af61a0e25ba509e1bb954e993
Peter Katz <peter.nichols.katz@gmail.com>
2022-10-12 00:55:39 -0400

    comment out function

--- rps.js
+++ rps.js
@@ -1,21 +1,36 @@
 const myArray = ["Rock","Paper","Scissors"];
 
-const playerSelection  = prompt("Rock, Paper, or Scissors?", " ");
-console.log(playerSelection);
-
 function getComputerChoice () {
     return myArray[Math.floor(Math.random() * myArray.length)];
 }
 console.log(getComputerChoice());
 
+let playerSelection = "Rock";
+console.log(playerSelection);
+
+let computerSelection = getComputerChoice();
+console.log(playRound());
+
 function playRound(playerSelection, computerSelection){
+    //case insensitive
     computerSelection = getComputerChoice().toLowerCase();
     playerSelection = playerSelection.toLowerCase();
 
-    if(playerSelection == computerSelection){
-        alert(
-            `It's a tie!`
-        );
-    }
+    if (playerSelection === computerSelection){
+        //tie
+         return alert(`It's a tie!`);
+    } else if (
+        //player wins
+        playerSelection === "Rock" || 
+        computerSelection === "Scissors";
+    ) {
+         alert(`Player wins!`);
+    } else (
+        //computer wins
+        playerSelection = "Rock" && computerSelection = "Paper";
+    ) {
+       alert (`Computer wins!`);
+    };
 
 }
+
