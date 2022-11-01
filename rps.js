0472fc3137a389de8ffd74c03e409ab0d9dc8b1b
Peter Katz <peter.nichols.katz@gmail.com>
2022-11-01 15:37:06 -0400

    update function playRound and for loop, achieve desired outcome

--- rps.js
+++ rps.js
@@ -1,55 +1,57 @@
-
-
 let playerScore = 0;
 let computerScore = 0;
 
+//computer choice
+const myArray = ["rock","paper","scissors"];
+
+function getComputerChoice () {
+return myArray[Math.floor(Math.random() * myArray.length)];
+}
+
+//rock paper scissors function
 function playRound(){
-
-    let playerInput = prompt("Rock, Paper, or Scissors?", " ");
-    let playerSelection = playerInput.toLowerCase();
-    console.log(playerSelection);
-
-    const myArray = ["rock","paper","scissors"];
-
-    function getComputerChoice () {
-    return myArray[Math.floor(Math.random() * myArray.length)];
-    }
-
-    let computerSelection = getComputerChoice();
+    
+    //computer choice
+    const computerSelection = getComputerChoice();
     console.log(computerSelection);
 
-    if ((playerSelection === computerSelection)){
+    //player choice
+    let playerInput = prompt("Rock, Paper, or Scissors?", " ");
+    const playerSelection = playerInput.toLowerCase();
+    console.log(playerSelection);
+
+    if (playerSelection == computerSelection){
         //tie
-        alert(`It's a tie!`);
+       alert(`It's a tie!`);
     } else if (
         (playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "scissors" && computerSelection === "paper")
     ) {
         //player wins
-        
-        alert(`Player wins!`);
         playerScore += 1;
-        console.log(`The player has ${playerScore} point(s).`);
-
-
+        return (`Player wins! The player has ${playerScore} point(s).`);
     } else {
         //computer wins
-        
-        alert (`Computer wins!`);
         computerScore += 1;
-        console.log(`The computer has ${computerScore} point(s).`);
-        
+        return (`Computer wins! The computer has ${computerScore} point(s).`);
     }
     };
-    
+
     function game() {
-        for (let playerScore = 0; playerScore <= 5; playerScore++) {
-            playRound();
+        for (let rounds = 0; (playerScore < 5) && (computerScore < 5); rounds ++) {
+            alert(playRound());
+        }
+        if (playerScore === 5){
+            alert("Player beat the computer!");
+        } else if (computerScore === 5) {
+            alert("Computer beat the player!");
+        } else {
+            alert("What have you done??")
         }
     };
 
-    game(); 
+     game(); 
 
 
 
